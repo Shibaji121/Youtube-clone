148bda90dec5abe500afbe18761eac639da464a2
shiba_1234 <shibajisahu121@gmail.com>
2023-07-23 15:13:39 +0530

    config file deleted

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAOM2CayuP3GJgX40nUMw--Y3G8Jzv8nnA",
-  authDomain: "clone-e5851.firebaseapp.com",
-  projectId: "clone-e5851",
-  storageBucket: "clone-e5851.appspot.com",
-  messagingSenderId: "15173531044",
-  appId: "1:15173531044:web:ad4dd13ae790eb68377d31",
-  measurementId: "G-CKF1NFFH9T",
-};
