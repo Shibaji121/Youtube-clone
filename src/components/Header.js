3a9be148d9c281c35a57daa12e472b6a00459a4b
shiba_1234 <shibajisahu121@gmail.com>
2023-07-23 18:45:01 +0530

    header Styles are added

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,26 +7,34 @@ import {
 } from "@mui/icons-material";
 import { Avatar } from "@mui/material";
 import React from "react";
+import "../styles/Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <h1>I am a Header</h1>
-      <Menu />
-      <img
-        className="header_logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/3/34/YouTube_logo_%282017%29.png"
-        alt="logo"
-      />
-      <input type="text" />
-      <Search />
-      <KeyboardVoiceRounded />
-      <VideoCallOutlined />
-      <NotificationsOutlined />
-      <Avatar
-        src="https://assets.turbologo.com/blog/en/2019/10/19084946/spiderman-logo-illustration-958x575.jpg"
-        alt="channel-logo"
-      />
+      <div className="header_left">
+        <Menu />
+        <img
+          className="header_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/3/34/YouTube_logo_%282017%29.png"
+          alt="logo"
+        />
+      </div>
+      <div className="header_middle">
+        <div className="header_input">
+          <input type="text" placeholder="Search" />
+          <Search className="search-icon" />
+        </div>
+        <KeyboardVoiceRounded className="voice-icon" />
+      </div>
+      <div className="header_right">
+        <VideoCallOutlined />
+        <NotificationsOutlined />
+        <Avatar
+          src="https://assets.turbologo.com/blog/en/2019/10/19084946/spiderman-logo-illustration-958x575.jpg"
+          alt="channel-avatar"
+        />
+      </div>
     </div>
   );
 }
