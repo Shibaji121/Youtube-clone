f5446d41743ecbfd28c38a2bdf47dc05cfa7574b
shiba_1234 <shibajisahu121@gmail.com>
2023-10-05 23:20:07 +0530

    Categories bar designed

--- src/components/CategoriesBar/CategoriesBar.js
+++ src/components/CategoriesBar/CategoriesBar.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import "../CategoriesBar/CategoriesBar.css";
+
+const keywords = [
+  "All",
+  "React JS",
+  "Angular",
+  "React Native",
+  "Use of Api",
+  "Redux",
+  "Music",
+  "Alogorithm art",
+  "Guitar",
+  "Bengoli Songs",
+  "Coding",
+  "Cricket",
+  "FootBall",
+  "Real Madrid",
+  "Shibaji",
+  "Sahu",
+  "Amul",
+  "Artists",
+];
 
 const CategoriesBar = () => {
+  const [activeElement, setActiveElement] = useState("All");
+
+  const handleClick = (value) => {
+    setActiveElement(value);
+  };
+
   return (
-    <div>
-      <h3>Chips are coming Soon WIP</h3>
+    <div className="cata-bar">
+      {keywords.map((value, index) => {
+        return (
+          <span
+            onClick={() => handleClick(value)}
+            key={index}
+            className={activeElement === value ? "active" : ""}
+          >
+            {value}
+          </span>
+        );
+      })}
     </div>
   );
 };
