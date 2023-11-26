4db457233e933d5faf2d72dea85e29b3a814c2cf
shiba_1234 <shibajisahu121@gmail.com>
2023-11-27 00:48:29 +0530

    final try build

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
-    key: process.env.REACT_APP_apiKey,
+    key: process.env.REACT_APP_API_KEY,
   },
 });
 
